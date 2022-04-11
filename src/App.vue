ed20609524d49676ae1f101f3b3a6c05ad27af10
hjseong <hjseong1222@gmail.com>
2022-04-11 13:08:31 +0900

    'vuex-1'

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,38 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- $store.state.count를 통해서 바로 값을 들고 올수 있다 -->
+    <h1>카운터에서 가져온 값입니다 {{count}}</h1>
+    <button @click="$store.commit('addcount')">추가</button>
+    <button @click="addcount">추가</button>
+
+    <!-- 버튼을 누르면 1 감소하는 버튼을 추가해주세요 -->
+    <button @click="$store.commit('subcount')">감소</button>
+
+    <ul>
+      <li 
+      v-for="list in $store.state.list" 
+      :key="list.id"
+      >
+        {{list.memo}}
+      </li>
+    </ul>
+
+    <!-- index.js에 name : ['홍길동','성춘향','그린']을 추가하고 -->
+    <!-- 아래에 computed를 이용하여 간략하게 만들어 이름을 출력하세요 -->
+    <ul>
+      <li 
+      v-for="(name, i) in names"
+      :key="i"
+      >
+        {{name}}
+      </li>
+    </ul>
+
+    <p>{{ doneTodosCount }}</p>
+    <p>{{doneTodoCountGetter}}</p>
+
+    <hello-world></hello-world>
+
   </div>
 </template>
 
@@ -10,8 +41,29 @@ import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  components : {
+    HelloWorld 
+  },
+  computed : {
+    //머스테치안의 내용이 길어지면 computed를 통해서 출력해줄수있다
+    count :function(){
+      return this.$store.state.count;
+    },
+    names : function() {
+      return this.$store.state.name;
+    },
+    doneTodosCount : function(){
+      return this.$store.state.todos.filter( todo => todo.done ).length
+    },
+    doneTodoCountGetter : function() {
+      // store getters를 통해 계산 된 내용을 컴포넌트마다 가져와서 사용 
+      return this.$store.getters.doneTodosCountGetter
+    }
+  },
+  methods : {
+    addcount : function() {
+      this.$store.commit('addcount')
+    }
   }
 }
 </script>
--- src/store/index.js
+++ src/store/index.js
@@ -1,17 +1,48 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from "vue";
+import Vuex from "vuex";
 
-Vue.use(Vuex)
+Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
+    // 값을 저장해서 가져오는 공간
+    count: 10,
+    list: [
+      { id: 1, memo: "a" },
+      { id: 2, memo: "b" },
+    ],
+    name: ["홍길동", "성춘향", "그린"],
+    todos: [
+      { id: 1, text: "...", done: true },
+      { id: 2, text: "...", done: false },
+    ],
   },
   getters: {
+    doneTodosCountGetter: function (state) {
+      // 동일한 형식으로 사용할수 있다
+      // filter( function(배열의 요소를 담는 변수) { return 참일때 요소값을 배열에 추가 } )
+      return state.todos.filter((todo) => todo.done).length;
+    },
+    todosReverse: function (state) {
+      return state.todos.reverse();
+    },
+    // todo의 id값을 통해 todo객체 반환
+    getTodoById: (state) => (id) => {
+      return state.todos.find((todo) => todo.id === id);
+    },
   },
   mutations: {
+    // data의 methods와 비슷한 역할
+    addcount: function (state) {
+      state.count++;
+    },
+    subcount: function (state) {
+      state.count--;
+    },
+    ncount: function (state, n) {
+      state.count += n;
+    },
   },
-  actions: {
-  },
-  modules: {
-  }
-})
+  actions: {},
+  modules: {},
+});
